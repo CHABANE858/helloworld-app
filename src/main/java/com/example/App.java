c1bcd44c7999c92c9acedd4fd8e55291e39d730b
CHABANE858 <bchabane97@outlook.fr>
2026-01-05 13:53:54 +0100

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,8 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class AppTest {
-
-    @Test
-    void testApp() {
-        assertTrue(true);
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
+
